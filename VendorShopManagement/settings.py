dd560bd650863e93f9eb72926fc491eb23ebdd0f
agiabhishek <abhishekxxxybl@gmail.com>
2025-03-18 16:12:05 +0530

    Updated settings.py for deployment

--- VendorShopManagement/settings.py
+++ VendorShopManagement/settings.py
@@ -1,14 +1,14 @@
-from pathlib import Path
+import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Database configuration
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',  # Fixed typo
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL', 'sqlite:///db.sqlite3')
+    )
 }
 
 # Installed apps
